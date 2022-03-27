3efe562523fbcd18192b68b3d57a0cb3d25b551b
Namrata <namrata.krmkr@gmail.com>
2022-03-28 01:45:07 +0530

    changes for heroku deployment

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { config } from "./config/config";
 import { app } from "./app";
-app.listen(process.env.PORT || config.get("port"), () => {
-  if(process.env.PORT) console.log(`${process.env.PORT}`);
-  else console.log(`listening at port ${config.get("port")}`);  
+
+const port = process.env.PORT || config.get("port")
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);  
 });
