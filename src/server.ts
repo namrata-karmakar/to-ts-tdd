a9d6975d2c178811238b8e3551990d9c96e14f68
Namrata <namrata.krmkr@gmail.com>
2022-04-11 15:14:06 +0530

    changed default port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import { app } from "./app";
+import express from "express";
+import path from "path";
+const port = process.env.PORT || 3012
 
-const port = process.env.PORT || 8080
+app.use(express.static(`${__dirname}/dist` ));
+
+app.get('/', async (req, res) =>  {
+  res.sendFile(path.join(`${__dirname}/dist/index.html`));
+})
+
 app.listen(port, () => {
   console.log(`listening at port ${port}`);
   console.log(`environment is ${process.env.NODE_ENV}`);
